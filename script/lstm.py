8e15bd18143a89684c42aaa940a3e9cb13e57f4b
93chungil <93chungil@gmail.com>
2022-11-06 09:45:48 -0500

    lstm run

--- script/lstm.py
+++ script/lstm.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import numpy as np  
 import pandas as pd 
 from sklearn.model_selection import train_test_split
@@ -8,9 +9,21 @@ from keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.layers import Input, LSTM, Embedding, Dense, TimeDistributed, Conv1D, MaxPooling1D
 from tensorflow.keras.models import Model
 from tensorflow.keras.callbacks import EarlyStopping
-import random
-import warnings
+import csv
+from rouge import Rouge
 
+def embedding_map(embedding_type):
+    if embedding_type == 'glove':
+        return 'glove.6B.300d.txt', 300
+    elif embedding_type == 'law2vec':
+        return 'Law2Vec.200d.txt', 200
+
+embedding_type = sys.argv[0]
+embedding_file, embed_dim = embedding_map(embedding_type)
+epochs = sys.argv[1]
+
+
+print("Embedding type: " + embedding_type)
 
 corpus_dir = os.path.join(os.path.dirname(os.getcwd()), 'corpus')
 
@@ -19,12 +32,14 @@ trimmed_df = pd.read_csv(os.path.join(corpus_dir, 'fulltext.csv'))
 maxlen_text = 10000
 maxlen_summ = 700
 
-test_size = 0.8
+test_size = 0.2
 
 train_x, test_val_x, train_y, test_val_y = train_test_split(trimmed_df['cleaned_text'], trimmed_df['cleaned_summary'], test_size=test_size, random_state=0)
 val_x, test_x, val_y, test_y = train_test_split(test_val_x, test_val_y, test_size=0.5, random_state=123)
 del trimmed_df
 
+og_test_x= test_x
+og_test_y = test_y
 
 t_tokenizer = Tokenizer()
 t_tokenizer.fit_on_texts(list(train_x))
@@ -73,17 +88,21 @@ t_tokenizer = Tokenizer(num_words=t_max_features)
 t_tokenizer.fit_on_texts(list(train_x))
 train_x = t_tokenizer.texts_to_sequences(train_x)
 val_x = t_tokenizer.texts_to_sequences(val_x)
+test_x = t_tokenizer.texts_to_sequences(test_x)
 
 train_x = pad_sequences(train_x, maxlen=maxlen_text, padding='post')
 val_x = pad_sequences(val_x, maxlen=maxlen_text, padding='post')
+test_x = pad_sequences(test_x, maxlen=maxlen_text, padding='post')
 
 s_tokenizer = Tokenizer(num_words=s_max_features)
 s_tokenizer.fit_on_texts(list(train_y))
 train_y = s_tokenizer.texts_to_sequences(train_y)
 val_y = s_tokenizer.texts_to_sequences(val_y)
+test_y = s_tokenizer.texts_to_sequences(test_y)
 
 train_y = pad_sequences(train_y, maxlen=maxlen_summ, padding='post')
 val_y = pad_sequences(val_y, maxlen=maxlen_summ, padding='post')
+test_y = pad_sequences(test_y, maxlen=maxlen_summ, padding='post')
 
 print("Training Sequence", train_x.shape)
 print('Target Values Shape', train_y.shape)
@@ -91,8 +110,7 @@ print('Test Sequence', val_x.shape)
 print('Target Test Shape', val_y.shape)
 
 embedding_index = {}
-embed_dim = 300
-with open('../embeddings/glove.42B.300d.txt', 'r', encoding='utf-8') as f:
+with open(f'../embeddings/{embedding_file}', 'r', encoding='utf-8') as f:
     for line in f:
         values = line.split()
         word = values[0]
@@ -112,13 +130,12 @@ for word, i in s_tokenizer.word_index.items():
         s_embed[i] = vec
 
 
-latent_dim = 128
 filters = 64
 kernel_size = 5
 pool_size = 4
 lstm_output_size = 200
 
-encoder_inputs = Input(shape=(maxlen_text,), dtype='float')
+encoder_inputs = Input(shape=(maxlen_text,))
 encoder_embedding = Embedding(t_max_features, embed_dim, input_length=maxlen_text, 
                                 weights=[t_embed], trainable=False)(encoder_inputs)
 encoder_cnn = Conv1D(filters,
@@ -130,7 +147,7 @@ encoder_maxpool = MaxPooling1D(pool_size=pool_size)(encoder_cnn)
 encoder_lstm = LSTM(lstm_output_size, return_state=True)
 encoder_outputs, state_h, state_c = encoder_lstm(encoder_maxpool)
 
-decoder_inputs = Input(shape=(None,), dtype='int32')
+decoder_inputs = Input(shape=(None,))
 decoder_embedding = Embedding(s_max_features, embed_dim, 
                                 weights=[s_embed], trainable=False)(decoder_inputs)
 decoder_lstm = LSTM(lstm_output_size, return_state=True, return_sequences=True)
@@ -151,38 +168,56 @@ model.fit([train_x, train_y[:, :-1]],
             verbose=2, 
             validation_data=([val_x, val_y[:, :-1]], 
             val_y.reshape(val_y.shape[0], val_y.shape[1], 1)[:, 1:]))
-# model = Sequential()
-# model.add(Embedding(t_max_features, embed_dim, inputh_length=maxlen_text, weights=[t_embed], trainable=False))
-# model.add(Conv1D)
-# model.add(MaxPooling1D(pool_size=pool_size))
-# model.add(LSTM(lstm_output_size))
-# model.add(Dense(1))
-# # Encoder
-# enc_input = Input(shape=(maxlen_text, ))
-# enc_embed = Embedding(t_max_features, embed_dim, input_length=maxlen_text, weights=[t_embed], trainable=False)(enc_input)
-# # h_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
-# # h_out, _, _ = h_lstm(enc_embed)
-# enc_lstm = Bidirectional(LSTM(latent_dim, return_state=True))
-# enc_output, enc_fh, enc_fc, enc_bh, enc_bc = enc_lstm(enc_embed)
-# enc_h = Concatenate(axis=-1, name='enc_h')([enc_fh, enc_bh])
-# enc_c = Concatenate(axis=-1, name='enc_c')([enc_fc, enc_bc])
-# #Decoder
-# dec_input = Input(shape=(None, ))
-# dec_embed = Embedding(s_max_features, embed_dim, weights=[s_embed], trainable=False)(dec_input)
-# dec_lstm = LSTM(latent_dim*2, return_sequences=True, return_state=True, dropout=0.3, recurrent_dropout=0.2)
-# dec_outputs, _, _ = dec_lstm(dec_embed, initial_state=[enc_h, enc_c])
 
-# dec_dense = TimeDistributed(Dense(s_max_features, activation='softmax'))
-# dec_output = dec_dense(dec_outputs)
+model.save(f'./{embedding_type}_model')
 
-# model = Model([enc_input, dec_input], dec_output)
-# model.summary()
+enc_model = Model(inputs=encoder_inputs, outputs=[state_h, state_c])
 
-# plot_model(
-#     model,
-#     to_file='./seq2seq_encoder_decoder.png',
-#     show_shapes=True,
-#     show_layer_names=True,
-#     rankdir='TB',
-#     expand_nested=False,
-#     dpi=96)
+dec_init_state_h = Input(shape=(lstm_output_size, ))
+dec_init_state_c = Input(shape=(lstm_output_size, ))
+
+dec_out, dec_h, dec_c = decoder_lstm(decoder_embedding, initial_state=[dec_init_state_h, dec_init_state_c])
+
+dec_final = TimeDistributed(Dense(s_max_features, activation='softmax'))(dec_out)
+dec_model = Model([decoder_inputs]+[dec_init_state_h, dec_init_state_c], [dec_final]+[dec_h, dec_c])
+
+def generate_summary(input_seq):
+    h, c = enc_model.predict(input_seq)
+    
+    next_token = np.zeros((1, 1))
+    next_token[0, 0] = s_tokenizer.word_index['sostok']
+    output_seq = ''
+    
+    stop = False
+    count = 0
+    
+    while not stop:
+        if count > 100:
+            break
+        decoder_out, state_h, state_c = dec_model.predict([next_token]+[h, c])
+        token_idx = np.argmax(decoder_out[0, -1, :])
+        
+        if token_idx == s_tokenizer.word_index['eostok']:
+            stop = True
+        elif token_idx > 0 and token_idx != s_tokenizer.word_index['sostok']:
+            token = s_tokenizer.index_word[token_idx]
+            output_seq = output_seq + ' ' + token
+        
+        next_token = np.zeros((1, 1))
+        next_token[0, 0] = token_idx
+        h, c = state_h, state_c
+        count += 1
+        
+    return output_seq
+
+hyps = []
+with open(f'./{embedding_type}_result.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(['Article', 'Original Summary', 'Model Output'])
+    for i in range(len(test_x)):
+        our_summ = generate_summary(test_x[i].reshape(1, maxlen_text))
+        hyps.append(our_summ)
+        writer.writerow([og_test_x[i], og_test_y[i], our_summ])
+
+rouge = Rouge()
+rouge.get_scores(hyps, og_test_y, avg=True, ignore_empty=True)
